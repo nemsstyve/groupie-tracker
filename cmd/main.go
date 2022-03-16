90f4779d33e4da61c70953d1f9009caa2e977dfa
Styve NEMATCHA <92915196+nemsstyve@users.noreply.github.com>
2022-03-16 12:49:30 +0100

    Create main.go
    new pull

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/nemsstyve/groupie-tracker/controller"
+	"github.com/julienschmidt/httprouter"
+)
+
+//
+const (
+	APIUpdateTime = 24 * time.Hour
+)
+
+func main() {
+	go api()
+
+	r := httprouter.New()
+	routes(r)
+
+	const PORT = ":8080"
+
+	fmt.Printf("Listening on the port %v\nhttp://localhost%v/\n", PORT, PORT)
+	log.Fatal(http.ListenAndServe(PORT, r))
+}
+
+func routes(r *httprouter.Router) {
+	r.ServeFiles("/public/*filepath", http.Dir("public"))
+
+	r.GET("/", controller.MainPage)
+	r.GET("/Artist/", controller.ArtistPage)
+	r.GET("/Artist/:id", controller.ArtistPage)
+	r.GET("/Info", controller.InfoPage)
+	r.POST("/Search", controller.SearchPage)
+	r.POST("/Filter", controller.FilterPage)
+}
+
+func api() {
+	for {
+		err := controller.Parse()
+		if err != nil {
+			log.Println(err)
+		}
+		time.Sleep(APIUpdateTime)
+	}
+}
